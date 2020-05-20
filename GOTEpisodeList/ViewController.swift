9c909a7fdaba3953069d6f04a308dc2e235ac858
melinda <melindad_82@msn.com>
2020-05-20 16:05:35 -0400

    Initial Commit

---
+++ GOTEpisodeList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GOTEpisodeList
+//
+//  Created by Melinda Diaz on 5/20/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
